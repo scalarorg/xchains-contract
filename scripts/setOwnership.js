fb9a668f7b5a6cb7c60509eabfd4b08236c0f500
dascy <dung.truongscy@hcmut.edu.vn>
2024-08-22 16:32:21 +0700

    docs: Update README

--- scripts/setOwnership.js
+++ scripts/setOwnership.js
@@ -17,23 +17,8 @@ async function main() {
     deployer
   );
 
-  //   const SBTC = await ethers.getContractFactory("sBTC");
-  //   const sbtc = await SBTC.deploy();
-  //   await sbtc.deployed();
-
   console.log("sbtc address:", sbtc.address);
   console.log("sbtc total supply:", await sbtc.totalSupply());
-
-  //   // Deploy the MintContract
-  //   const gatewayAddress = "0x70b9E1B98fb9cDd0221778c1E4d72e7a386D9CCe";
-  //   const gasServiceAddress = "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6";
-  //   const MintContract = await ethers.getContractFactory("MintContract");
-  //   const mintContract = await MintContract.deploy(
-  //     gatewayAddress,
-  //     gasServiceAddress,
-  //     sbtc.address
-  //   );
-  //   await mintContract.deployed();
 
   const mintContractName = "MintContract";
   const mintContractArtifact = require(`../artifacts/contracts/${mintContractName}.sol/${mintContractName}.json`);
@@ -51,61 +36,6 @@ async function main() {
   );
   await txTransferOwnership.wait();
   console.log("sbtc owner:", await sbtc.owner());
-  //   const oneSBTC = ethers.utils.parseUnits("1", 18);
-  //   const txMint = await mintContract.mintTest(deployer.address, oneSBTC);
-  //   await txMint.wait();
-  //   console.log("deployer sbtc balance:", await sbtc.balanceOf(deployer.address));
-  //   console.log(
-  //     "mintContract sbtc balance:",
-  //     await sbtc.balanceOf(mintContract.address)
-  //   );
-  //   const txTransferOwnershipAgain = await mintContract.transferMintOwnership(
-  //     deployer.address
-  //   );
-  //   await txTransferOwnershipAgain.wait();
-  //   console.log("sbtc owner:", await sbtc.owner());
-
-  //   saveFrontendFiles([
-  //     {
-  //       name: "mintContract",
-  //       address: callContract.address,
-  //     },
-  //   ]);
-}
-function saveFrontendFiles(contracts) {
-  const fs = require("fs");
-  const contractsDir = path.join(
-    __dirname,
-    "..",
-    "frontend",
-    "src",
-    "abis",
-    "call"
-  );
-
-  if (!fs.existsSync(contractsDir)) {
-    fs.mkdirSync(contractsDir);
-  }
-
-  let contractAddresses = {};
-
-  contracts.forEach((contract) => {
-    // Save each contract's address
-    contractAddresses[contract.name] = contract.address;
-
-    // Save each contract's artifact
-    const ContractArtifact = artifacts.readArtifactSync(contract.name);
-    fs.writeFileSync(
-      path.join(contractsDir, `${contract.name}.json`),
-      JSON.stringify(ContractArtifact, null, 2)
-    );
-  });
-
-  // Save all contract addresses in a single file
-  fs.writeFileSync(
-    path.join(contractsDir, "contract-addresses.json"),
-    JSON.stringify(contractAddresses, undefined, 2)
-  );
 }
 
 main()
